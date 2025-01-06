5c7ab67e3c49cbc997f5fa6fa3443b97a1460745
Arvid Boisen <arvid@boisen.io>
2025-01-06 11:22:42 +0100

    added portainer webhook

--- push-image.sh
+++ push-image.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t registry.boisen.io/arvid.boisen/boisen.io-website:latest .
 docker push registry.boisen.io/arvid.boisen/boisen.io-website:latest
+curl -X POST https://portainer.boisen.io/api/webhooks/17adbf4a-e595-4e17-a39e-5d0e6730685c
