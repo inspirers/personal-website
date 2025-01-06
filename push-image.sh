f9e34997165788ee310c486bca5aeb973d34e044
Arvid Boisen <arvid@boisen.io>
2025-01-06 12:16:45 +0100

    build image when deploying

--- push-image.sh
+++ push-image.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+JEKYLL_ENV=production bundle exec jekyll b
 docker build -t registry.boisen.io/arvid.boisen/boisen.io-website:latest .
 docker push registry.boisen.io/arvid.boisen/boisen.io-website:latest
 curl -X POST https://portainer.boisen.io/api/webhooks/17adbf4a-e595-4e17-a39e-5d0e6730685c
