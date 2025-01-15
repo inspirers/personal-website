6dd067d0ba2a5d550c83d9911d4ec829443f1d9f
Arvid Boisen <arvid@boisen.io>
2025-01-15 21:47:28 +0100

    multiarch

--- push-image.sh
+++ push-image.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 JEKYLL_ENV=production bundle exec jekyll b
-docker build -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
-docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest --push .
 
-docker build --platform linux/arm64 -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
-docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
+
+# docker build -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
+# docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
+
+# docker build --platform linux/arm64 -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
+# docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
 # curl -X POST https://portainer.boisen.io/api/webhooks/17adbf4a-e595-4e17-a39e-5d0e6730685c
