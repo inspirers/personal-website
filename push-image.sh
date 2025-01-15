83f4bb3eb20844db0d0e05bb16e309d74ea47f97
Arvid Boisen <arvid@boisen.io>
2025-01-15 21:10:41 +0100

    arm64

--- push-image.sh
+++ push-image.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 JEKYLL_ENV=production bundle exec jekyll b
-docker build -t registry.boisen.io/arvid.boisen/boisen.io-website:latest .
-docker push registry.boisen.io/arvid.boisen/boisen.io-website:latest
-curl -X POST https://portainer.boisen.io/api/webhooks/17adbf4a-e595-4e17-a39e-5d0e6730685c
+docker build -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
+docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
+
+docker build --platform linux/arm64 -t registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest .
+docker push registry.boisen.io:8443/arvid.boisen/boisen.io-website:latest
+# curl -X POST https://portainer.boisen.io/api/webhooks/17adbf4a-e595-4e17-a39e-5d0e6730685c
